5603156c563acf0fd573313bf253348aa26ab55a
AnshChoudhary <142714177+Ansh-Choudhary-1@users.noreply.github.com>
2025-06-26 01:46:52 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "https://convo-amber.vercel.app/",
+    origin: "http://localhost:5173",
     credentials: true, // allow frontend to send cookies
   })
 );
